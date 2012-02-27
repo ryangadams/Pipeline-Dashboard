b61e6a924fcc1f9a516c6d2478288ac202a6505e
Ryan Adams <ryan.g.adams@gmail.com>
2012-02-27 23:43:20 +0000

    a neat little graph of lifecycle status (and some hideous jquery)

--- output/pipeline-json.js
+++ output/pipeline-json.js
@@ -1,10 +1,19 @@
+/*
+ * This javascript is a real mess. Tidy this up forthwith!
+ */
+
+
 function buildHTML(){
     var table = $('<table id="project-table"></table>');
     table.append($('<thead><tr><th>KEY</th><th>SUMMARY</th><th id="status-header">STATUS</th><th id="">LIFECYCLE</th><th>TPM</th><th>RAG</th><th>JIRA</th><th>CONFLUENCE</th></tr></thead>'));
     var tableBody = $("<tbody></tbody>");
     table.append(tableBody);
-    $.each(issues_json.keys, function(index, value){
-        var el = $('<tr id="row_' + index + '" class="' + value.rag + ' ' + value.status + '">' +
+    $.each(issues_json.keys, function(index, value){        
+				if (value.lifecycle == "") {
+					value.lifecycle = "unknown";
+				}
+        var el = $('<tr id="row_' + index + '" ' + 
+				'class="' + value.rag + ' ' + value.status + ' ' + value.lifecycle.toLowerCase() + '">' +
         '<td><a href="https://jira.dev.bbc.co.uk/browse/' +value.key +'">' + value.key + '</a></td>' +
         '<td class="summary">' + value.summary + '<span class="hidden">' + value.laststatus  + '</span></td>' +
         '<td>' + value.status + '</td>' +
@@ -25,6 +34,8 @@ function buildHTML(){
         tableBody.append(el);
     });
     $("#wrapper").append('<p><a href="' + issues_json.url + '">View List on JIRA</a></p>').append(table);
+	  
+	buildGraph();
     addEvents();
 }
 function addEvents() {
@@ -119,5 +130,47 @@ function addEvents() {
             $(this).text("Show Closed Projects");
         }
         $("body").toggleClass("hide-closed");
-    }) 
+    });
+ 		$("#graph div").hover(
+			function(){
+	  	$(this).css('cursor', 'pointer');
+			},
+			function(){
+		  	$(this).css('cursor', '');
+			}
+		).click(function(e){                  
+			status = $(this).attr("id").split("-")[1];
+			$("#project-table tr").removeClass("hidden");
+			$("#project-table tr:not('." + status + "')").addClass("hidden");
+		});     
+    $("#graph p a").click(function(e){
+			e.preventDefault();
+			$("#project-table tr").removeClass("hidden");
+		});
+}
+
+function buildGraph() { 
+	var columnOrder = ["concept", "feasibility", "discovery", "build", "run", "unknown"];
+	var columns = [];
+	var projectCount = 0;
+	$("table tr").each(function(index, element){
+		lifecycleStatus = $(this).find("td:nth-child(4)").text().toLowerCase();
+		if(lifecycleStatus != "") {
+			(columns[lifecycleStatus] == undefined) ? columns[lifecycleStatus] = 1 : columns[lifecycleStatus]++;
+		} else {                                                                                              
+			lifecycleStatus = "unknown";
+			(columns[lifecycleStatus] == undefined) ? columns[lifecycleStatus] = 1 : columns[lifecycleStatus]++;
+		}       
+		projectCount = index + 1;
+	});                     
+	var thisWidth = 0;
+	var thatWidth = 0;
+	var graph = $('<div id="graph"><p>Pipeline Overview </p></div>');
+	$.each(columnOrder, function(index, value){         
+		thatWidth = thatWidth + thisWidth;                 
+		thisWidth = (columns[value] / projectCount * 100);
+		graph.append('<div id="graph-'+value +'" style="width:' + thisWidth + '%;margin-left:' + thatWidth + '%;">' + value + ' ('+ columns[value] +')</div>');
+	});                                                              
+	$("table").before(graph);  
+	$("#graph p").append('<a href="#">Show All</a>');
 }
